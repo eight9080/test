3c0b50370cf1dec0a42824c0ba7087b146af4106
eight9080 <eight9080@gmail.com>
2017-10-02 01:22:01 +0300

    palindrome pairs

--- src/main/java/com/example/alg/stringpck/Palindrome.java
+++ src/main/java/com/example/alg/stringpck/Palindrome.java
@@ -1,5 +1,9 @@
 package com.example.alg.stringpck;
 
+import java.util.*;
+
+import static java.util.Arrays.asList;
+
 public class Palindrome {
 
     /**
@@ -19,6 +23,88 @@ public class Palindrome {
             if(s.charAt(i)!=s.charAt(s.length()-1-i)){
                 return false;
             }
+        }
+        return true;
+    }
+
+    /**
+     * Given a list of unique words. Find all pairs of distinct indices (i, j) in the given list,
+     * so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+
+     Example 1:
+     Given words = ["bat", "tab", "cat"]
+     Return [[0, 1], [1, 0]]
+     The palindromes are ["battab", "tabbat"]
+     */
+    public static List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        HashMap<String, Integer> mapIndex = new HashMap<>();
+        for(int i=0; i<words.length; i++){
+            mapIndex.put(words[i], i);
+        }
+
+        for(int i = 0; i< words.length; i++){
+            String word = words[i];
+
+            //if the word is a palindrome, get index of ""
+            if(isPalindrome(word)){
+                final Optional<Integer> pairWordIndex = findPairIndex("", i, mapIndex);
+                if(pairWordIndex.isPresent()){
+                    result.add(asList( i, pairWordIndex.get()));
+                    result.add(asList( pairWordIndex.get(), i));
+                }
+            }
+
+            final Optional<Integer> pairWordIndex = findPairIndex(word, i, mapIndex);
+            if(pairWordIndex.isPresent()){
+                result.add(asList( i, pairWordIndex.get()));
+            }
+
+            for(int k=1; k<word.length(); k++) {
+                String left = word.substring(0, k);
+                String right = word.substring(k);
+
+                if(isPalindrome(left)){
+                    final Optional<Integer> pairIndex = findPairIndex(right, i, mapIndex);
+                    if(pairIndex.isPresent()){
+                        result.add(asList( pairIndex.get(), i));
+                    }
+                }
+                if(isPalindrome(right)){
+                    final Optional<Integer> pairIndex = findPairIndex(left, i, mapIndex);
+                    if(pairIndex.isPresent()){
+                        result.add(asList( i, pairIndex.get()));
+                    }
+                }
+
+            }
+        }
+
+        return result;
+    }
+
+    private static Optional<Integer> findPairIndex(String word, Integer wordIndex, Map<String, Integer> mapIndex){
+        final String reverseWord = "".equals(word)? "" : new StringBuilder(word).reverse().toString();
+        if(mapIndex.containsKey(reverseWord)){
+            final Integer reverseWordIndex = mapIndex.get(reverseWord);
+            if(!Objects.equals(reverseWordIndex, wordIndex)){
+                return Optional.of(reverseWordIndex);
+            }
+        }
+        return Optional.empty();
+    }
+
+    private static boolean isPalindrome(String word) {
+        int i= 0;
+        int j = word.length()-1;
+
+        while (i<j){
+            if(word.charAt(i) != word.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
         }
         return true;
     }
--- src/test/java/com/example/alg/stringpck/PalindromeTest.java
+++ src/test/java/com/example/alg/stringpck/PalindromeTest.java
@@ -2,12 +2,33 @@ package com.example.alg.stringpck;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 
 public class PalindromeTest {
     @Test
     public void isValidPalindrome() throws Exception {
         assertTrue(Palindrome.isValidPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    @Test
+    public void palindromePairs(){
+        final List<List<Integer>> palindromePairs = Palindrome.palindromePairs(new String[]{"bat", "tab", "cat"});
+        assertEquals(asList(asList(0,1), asList(1,0)), palindromePairs);
+        assertEquals(asList(asList(0,2), asList(2,0)),
+                Palindrome.palindromePairs(new String[]{"bat", "cat", "tab"}));
+
+        assertEquals(asList(asList(0,2), asList(2,0), asList(1,2), asList(2,1)),
+                Palindrome.palindromePairs(new String[]{"bob", "lol", "", "abcd"}));
+
+        assertEquals(asList(asList(2,0)),
+                Palindrome.palindromePairs(new String[]{"lolbat", "cat", "tab"}));
+        assertEquals(asList(asList(1,2)),
+                Palindrome.palindromePairs(new String[]{"lol", "batlol", "tab"}));
     }
 
 }
