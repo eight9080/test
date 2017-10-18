ef0d9070b1465542dbb0d0f9558132c6f76464f7
eight9080 <eight9080@gmail.com>
2017-10-19 01:45:50 +0300

    remove duplicates array

--- src/main/java/com/example/alg/arraypkg/SortedArray.java
+++ src/main/java/com/example/alg/arraypkg/SortedArray.java
@@ -32,6 +32,45 @@ public class SortedArray {
 
     }
 
+    /**
+     * Follow up for "Remove Duplicates":
+     What if duplicates are allowed at most twice?
+
+     For example,
+     Given sorted array nums = [1,1,1,2,2,3],
+
+     Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3.
+     It doesn't matter what you leave beyond the new length.
+     */
+    public int removeDuplicates2(int[] nums) {
+        if(nums==null || nums.length==0){
+            return 0;
+        }
+        int extraDuplicateElement = 1;
+        if(nums.length<2){
+            return nums.length;
+        }
+        int noDuplicate = 0;
+        int position = 0;
+        int currentDuplicates=0;
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i]==nums[position]){
+                if(currentDuplicates<extraDuplicateElement){
+                    nums[++position]=nums[i];
+                    currentDuplicates++;
+                }else {
+                    noDuplicate++;
+                }
+
+            }else{
+                nums[++position]=nums[i];
+                currentDuplicates=0;
+            }
+
+        }
+        return nums.length-noDuplicate;
+    }
+
 
 
 }
--- src/test/java/com/example/alg/arraypkg/SortedArrayTest.java
+++ src/test/java/com/example/alg/arraypkg/SortedArrayTest.java
@@ -6,6 +6,21 @@ import static org.junit.Assert.*;
 
 public class SortedArrayTest {
     @Test
+    public void removeDuplicates2() throws Exception {
+
+        final SortedArray s = new SortedArray();
+
+        int[] input = {1,1,1,1,3,3};
+        assertEquals(4,s.removeDuplicates2(input));
+        assertArrayEquals(new int[]{1,1,3,3,3,3}, input);
+
+        input = new int[]{1, 1, 1, 2, 2, 3};
+        assertEquals(5,s.removeDuplicates2(input));
+        assertArrayEquals(new int[]{1,1,2,2,3,3}, input);
+
+    }
+
+    @Test
     public void removeDuplicates() throws Exception {
         final SortedArray s = new SortedArray();
         final int[] input = {1, 1, 2};
