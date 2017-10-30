c9a8a016bf4d73dc3fa890e6996b7bc29e63fe1f
eight9080 <eight9080@gmail.com>
2017-10-31 00:27:19 +0200

    add numbers

--- src/test/java/com/example/alg/listpck/AddNumbers.java
+++ src/test/java/com/example/alg/listpck/AddNumbers.java
@@ -1,27 +1,78 @@
 package com.example.alg.listpck;
 
+import java.util.Objects;
+
 /**
  * You are given two non-empty linked lists representing two non-negative integers.
  * The digits are stored in reverse order and each of their nodes contain a single digit.
  * Add the two numbers and return it as a linked list.
-
- You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
- Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
- Output: 7 -> 0 -> 8
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
  */
 public class AddNumbers {
 
 
     public static class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
-  }
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        ListNode(int x, ListNode next) {
+            val = x;
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ListNode listNode = (ListNode) o;
+            return val == listNode.val &&
+                    Objects.equals(next, listNode.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, next);
+        }
+    }
 
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
 
-        return null;
+        ListNode resultHead = new ListNode(0);
+        ListNode n1 = l1;
+        ListNode n2 = l2;
+        ListNode n3  = resultHead;
+
+        while (n1 != null || n2 != null) {
+            int result = carry;
+
+            if(n1!=null) {
+                result += n1.val;
+                n1 = n1.next;
+            }
+            if(n2!=null) {
+                result += n2.val;
+                n2 = n2.next;
+            }
+
+            n3.next = new ListNode(result%10);
+            n3 = n3.next;
+
+            carry = result/10;
+        }
+        if(carry>0){
+            n3.next=new ListNode(carry);
+        }
+
+        return resultHead.next;
     }
 }
--- src/test/java/com/example/alg/listpck/AddNumbersTest.java
+++ src/test/java/com/example/alg/listpck/AddNumbersTest.java
@@ -7,6 +7,25 @@ import static org.junit.Assert.*;
 public class AddNumbersTest {
     @Test
     public void addTwoNumbers() throws Exception {
+        final AddNumbers.ListNode l1 = new AddNumbers.ListNode(2, new AddNumbers.ListNode(4, new AddNumbers.ListNode(3)));
+        final AddNumbers.ListNode l2 = new AddNumbers.ListNode(5, new AddNumbers.ListNode(6, new AddNumbers.ListNode(4)));
+        final AddNumbers.ListNode expected = new AddNumbers.ListNode(7, new AddNumbers.ListNode(0, new AddNumbers.ListNode(8)));
+
+        final AddNumbers a = new AddNumbers();
+
+        assertEquals(expected, a.addTwoNumbers(l1, l2));
+
+    }
+
+    @Test
+    public void addTwoNumbers_carryLastDigit() throws Exception {
+        final AddNumbers.ListNode l1 = new AddNumbers.ListNode(5);
+        final AddNumbers.ListNode l2 = new AddNumbers.ListNode(5);
+        final AddNumbers.ListNode expected = new AddNumbers.ListNode(0, new AddNumbers.ListNode(1));
+
+        final AddNumbers a = new AddNumbers();
+
+        assertEquals(expected, a.addTwoNumbers(l1, l2));
 
     }
 
