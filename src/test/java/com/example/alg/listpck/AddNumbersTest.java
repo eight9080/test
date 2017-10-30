3ff62fb943874a3ba32fb83313fef06054b4c721
eight9080 <eight9080@gmail.com>
2017-10-30 08:58:43 +0200

    add numbers partial

---
+++ src/test/java/com/example/alg/listpck/AddNumbersTest.java
@@ -0,0 +1,13 @@
+package com.example.alg.listpck;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddNumbersTest {
+    @Test
+    public void addTwoNumbers() throws Exception {
+
+    }
+
+}
