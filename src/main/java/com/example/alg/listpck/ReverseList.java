814f685cb8e40453202c1cc3c8aa9ae77a67101d
eight9080 <eight9080@gmail.com>
2017-11-06 09:53:27 +0200

    reverse list 2 partial

--- src/main/java/com/example/alg/listpck/ReverseList.java
+++ src/main/java/com/example/alg/listpck/ReverseList.java
@@ -24,4 +24,16 @@ public class ReverseList {
         }
         return l1;
     }
+
+    /**
+     * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+     For example: given 1->2->3->4->5->NULL, m = 2 and n = 4, return 1->4->3->2->5->NULL.
+     */
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+
+        if(m==n) return head;
+
+        return null;
+    }
 }
--- src/test/java/com/example/alg/listpck/ReverseListTest.java
+++ src/test/java/com/example/alg/listpck/ReverseListTest.java
@@ -1,5 +1,6 @@
 package com.example.alg.listpck;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -7,6 +8,21 @@ import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class ReverseListTest {
+    @Test
+    @Ignore
+    public void reverseBetween() throws Exception {
+        final ListNode list = ListNode.createList(Arrays.asList(1, 2,  3, 4));
+        ReverseList r = new ReverseList();
+
+        ListNode listNode = r.reverseBetween(list, 2, 4);
+
+        final ListNode expList = ListNode.createList(Arrays.asList(1, 4,3,2,5));
+
+        assertEquals(expList, listNode);
+
+
+    }
+
     @Test
     public void reverseList() throws Exception {
 
