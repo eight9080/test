e032cfab1069acc5f3c0763ab10d65378463c733
eight9080 <eight9080@gmail.com>
2017-12-23 00:25:29 +0200

    house robber

--- src/test/java/com/example/alg/dynamic/HouseRobber.java
+++ src/test/java/com/example/alg/dynamic/HouseRobber.java
@@ -1,5 +1,7 @@
 package com.example.alg.dynamic;
 
+import com.example.alg.treepck.TreeNode;
+
 public class HouseRobber {
     /**
      * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
@@ -42,5 +44,91 @@ public class HouseRobber {
         }
 
         return dp[nums.length-1];
+    }
+
+
+    /**
+     * After robbing those houses on that street, the thief has found himself a new place for his thievery so that he will not get too much attention. This time, all houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, the security system for these houses remain the same as for those in the previous street.
+
+     Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+     */
+    public int rob2(int[] nums) {
+        if(nums==null || nums.length==0)
+            return 0;
+
+        if(nums.length==1)
+            return nums[0];
+
+        int max1 = robDP(nums, 0, nums.length-2);
+        int max2 = robDP(nums, 1, nums.length-1);
+
+        return Math.max(max1, max2);
+
+    }
+
+    private int robDP(int[] nums, int start, int end) {
+        if(start==end){
+            return nums[start];
+        }
+
+        int[] robs = new int[nums.length];
+        robs[start] = nums[start];
+        robs[start+1] = Math.max(robs[start], nums[start+1]);
+
+        for(int i = start+2; i<=end; i++){
+            robs[i] = Math.max(robs[i-2]+nums[i], robs[i-1]);
+        }
+
+        return robs[end];
+
+    }
+
+    /**
+     * The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+     Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+     Example 1:
+     3
+     / \
+     2   3
+     \   \
+     3   1
+     Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+     Example 2:
+     3
+     / \
+     4   5
+     / \   \
+     1   3   1
+     Maximum amount of money the thief can rob = 4 + 5 = 9.
+     */
+    public int rob3(TreeNode root) {
+        if(root == null)
+            return 0;
+
+        int [] sumWithAndWithoutRoot = robSum(root);
+        return Math.max(sumWithAndWithoutRoot[0], sumWithAndWithoutRoot[1]);
+
+    }
+
+    private int[] robSum(TreeNode root) {
+
+        if(root == null){
+            int[] result = {0, 0};
+            return result;
+        }
+
+
+        int[] result = new int[2];
+
+        final int[] leftSum = robSum(root.left);
+        final int[] rightSum = robSum(root.right);
+
+        // 0 is when root is selected, 1 is when not
+        result[0] = root.val + leftSum[1]+rightSum[1];
+        result[1] = Math.max(leftSum[0], leftSum[1]) + Math.max(rightSum[0], rightSum[1]);
+
+        return result;
     }
 }
--- src/test/java/com/example/alg/dynamic/HouseRobberTest.java
+++ src/test/java/com/example/alg/dynamic/HouseRobberTest.java
@@ -1,10 +1,39 @@
 package com.example.alg.dynamic;
 
+import com.example.alg.treepck.TreeNode;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HouseRobberTest {
+    @Test
+    public void rob3() throws Exception {
+        final HouseRobber h = new HouseRobber();
+        assertEquals(7, h.rob3(tree()));
+    }
+
+    private TreeNode tree(){
+        TreeNode root = new TreeNode(3);
+        TreeNode node11 = new TreeNode(2);
+        TreeNode node12 = new TreeNode(3);
+        root.left=node11;
+        root.right=node12;
+        TreeNode node21 = new TreeNode(3);
+        node11.left=node21;
+
+        TreeNode node31 = new TreeNode(1);
+        node12.left=node31;
+
+        return root;
+    }
+
+    @Test
+    public void rob2() throws Exception {
+        final HouseRobber h = new HouseRobber();
+        assertEquals(3, h.rob2(new int[]{2,1,1,2}));
+
+    }
+
     @Test
     public void robDP() throws Exception {
         final HouseRobber h = new HouseRobber();
