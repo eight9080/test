5f5727190c60c411e3ad70c5c560327afee41958
eight9080 <eight9080@gmail.com>
2018-01-06 21:39:36 +0200

    coin change 2

--- src/main/java/com/example/alg/dynamic/CoinChange.java
+++ src/main/java/com/example/alg/dynamic/CoinChange.java
@@ -1,5 +1,8 @@
 package com.example.alg.dynamic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CoinChange {
     /**
      * You are given coins of different denominations and a total amount of money amount.
@@ -47,6 +50,76 @@ public class CoinChange {
 
         return dp[amount];
 
-
     }
+
+    /**
+     * You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+
+     Note: You can assume that
+
+     0 <= amount <= 5000
+     1 <= coin <= 5000
+     the number of coins is less than 500
+     the answer is guaranteed to fit into signed 32-bit integer
+     Example 1:
+
+     Input: amount = 5, coins = [1, 2, 5]
+     Output: 4
+     Explanation: there are four ways to make up the amount:
+     5=5
+     5=2+2+1
+     5=2+1+1+1
+     5=1+1+1+1+1
+     Example 2:
+
+     Input: amount = 3, coins = [2]
+     Output: 0
+     Explanation: the amount of 3 cannot be made up just with coins of 2.
+     Example 3:
+
+     Input: amount = 10, coins = [10]
+     Output: 1
+
+     */
+    public int change(int amount, int[] coins) {
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <=amount ; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+
+    public int change2(int amount, int[] coins) {
+        return change2(amount, coins, 0, new HashMap<>());
+    }
+
+    public int change2(int amount, int[] coins, int index,  Map<String, Integer> memo) {
+        if(amount==0){
+            return 1;
+        }
+        if(index>=coins.length){
+            return 0;
+        }
+
+        String key = amount+"-"+index;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+
+        int amountWithCoin = 0;
+        int ways = 0;
+        while (amountWithCoin<=amount){
+            int remaining = amount - amountWithCoin;
+            ways += change2(remaining, coins, index+1, memo);
+            amountWithCoin+=coins[index];
+        }
+        memo.put(key, ways);
+        return ways;
+    }
+
 }
--- src/test/java/com/example/alg/dynamic/CoinChangeTest.java
+++ src/test/java/com/example/alg/dynamic/CoinChangeTest.java
@@ -6,6 +6,23 @@ import static org.junit.Assert.*;
 
 public class CoinChangeTest {
     @Test
+    public void change() throws Exception {
+        final CoinChange c = new CoinChange();
+        assertEquals(4, c.change(5, new int[]{1,2,5}));
+        assertEquals(0, c.change(3, new int[]{2}));
+        assertEquals(1, c.change(10, new int[]{10}));
+    }
+
+    @Test
+    public void change2() throws Exception {
+        final CoinChange c = new CoinChange();
+        assertEquals(4, c.change2(5, new int[]{1,2,5}));
+        assertEquals(0, c.change2(3, new int[]{2}));
+        assertEquals(1, c.change2(10, new int[]{10}));
+    }
+
+
+    @Test
     public void coinChange() throws Exception {
         final CoinChange c = new CoinChange();
 
