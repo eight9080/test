ea20c7b9f53d6e3e4454cb3e7b7eb611aa47f740
eight9080 <eight9080@gmail.com>
2018-01-22 23:53:44 +0200

    super ugly number

--- src/main/java/com/example/alg/mathpck/UglyNumber.java
+++ src/main/java/com/example/alg/mathpck/UglyNumber.java
@@ -1,7 +1,6 @@
 package com.example.alg.mathpck;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class UglyNumber {
     /**
@@ -69,6 +68,48 @@ public class UglyNumber {
             }
         }
 
+        return numbers.get(n-1);
+
+    }
+
+    /**
+     * Write a program to find the nth super ugly number.
+
+     Super ugly numbers are positive numbers whose all prime factors are in the given prime list primes of size k.
+     For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is
+     the sequence of the first 12 super ugly numbers given
+     primes = [2, 7, 13, 19] of size 4.
+
+     Note:
+     (1) 1 is a super ugly number for any given primes.
+     (2) The given numbers in primes are in ascending order.
+     (3) 0 < k ≤ 100, 0 < n ≤ 106, 0 < primes[i] < 1000.
+     (4) The nth super ugly number is guaranteed to fit in a 32-bit signed integer.
+     */
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        if(n<=0){
+            return 0;
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+
+        int[] indexes = new int[primes.length];
+
+        for (int i = 0; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < primes.length; j++) {
+                min=Math.min(min, numbers.get(indexes[j])*primes[j]);
+            }
+            numbers.add(min);
+
+            for (int j = 0; j < primes.length; j++) {
+                if(min == numbers.get(indexes[j])*primes[j]){
+                    indexes[j]++;
+                }
+            }
+
+        }
         return numbers.get(n-1);
 
     }
--- src/test/java/com/example/alg/mathpck/UglyNumberTest.java
+++ src/test/java/com/example/alg/mathpck/UglyNumberTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.*;
 
 public class UglyNumberTest {
     @Test
+    public void nthSuperUglyNumber() throws Exception {
+        final UglyNumber u = new UglyNumber();
+        assertEquals(32, u.nthSuperUglyNumber(12, new int[]{2, 7, 13, 19}));
+    }
+
+    @Test
     public void nthUglyNumber() throws Exception {
         final UglyNumber u = new UglyNumber();
         assertEquals(12, u.nthUglyNumber(10));
