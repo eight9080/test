bfd584151f5d7e430d50600cf647372639f1e5c2
eight9080 <eight9080@gmail.com>
2018-01-22 23:11:46 +0200

    ugly number 2

--- src/main/java/com/example/alg/mathpck/UglyNumber.java
+++ src/main/java/com/example/alg/mathpck/UglyNumber.java
@@ -1,5 +1,8 @@
 package com.example.alg.mathpck;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UglyNumber {
     /**
      * Write a program to check whether a given number is an ugly number.
@@ -25,5 +28,48 @@ public class UglyNumber {
             return isUgly(num);
         }
         return false;
+    }
+
+    /**
+     * Write a program to find the n-th ugly number.
+
+     Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+
+     Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
+     */
+    public int nthUglyNumber(int n) {
+
+        if(n<=0){
+            return 0;
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+
+        int index2 =0;
+        int index3 =0;
+        int index5 =0;
+
+        while (numbers.size()<n){
+            int num2=numbers.get(index2)*2;
+            int num3=numbers.get(index3)*3;
+            int num5=numbers.get(index5)*5;
+
+            final int min = Math.min(num2, Math.min(num3, num5));
+            numbers.add(min);
+
+            if(min==num2){
+                index2++;
+            }
+            if(min==num3){
+                index3++;
+            }
+            if(min==num5){
+                index5++;
+            }
+        }
+
+        return numbers.get(n-1);
+
     }
 }
--- src/test/java/com/example/alg/mathpck/UglyNumberTest.java
+++ src/test/java/com/example/alg/mathpck/UglyNumberTest.java
@@ -6,6 +6,13 @@ import static org.junit.Assert.*;
 
 public class UglyNumberTest {
     @Test
+    public void nthUglyNumber() throws Exception {
+        final UglyNumber u = new UglyNumber();
+        assertEquals(12, u.nthUglyNumber(10));
+
+    }
+
+    @Test
     public void isUgly() throws Exception {
         final UglyNumber u = new UglyNumber();
         assertTrue(u.isUgly(6));
