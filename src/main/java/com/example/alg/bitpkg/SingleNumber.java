bc58ba08f11ad59bcfe7bb5bab9839af0537542f
eight9080 <eight9080@gmail.com>
2018-01-28 23:04:49 +0200

    single number 2

--- src/main/java/com/example/alg/bitpkg/SingleNumber.java
+++ src/main/java/com/example/alg/bitpkg/SingleNumber.java
@@ -14,4 +14,22 @@ public class SingleNumber {
 
     }
 
+
+    /**
+     * Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+
+     */
+    public int singleNumber2(int[] nums) {
+        int ones = 0, twos = 0, threes = 0;
+        for (int i = 0; i < nums.length; i++) {
+            twos |= ones & nums[i];
+            ones ^= nums[i];
+            threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        return ones;
+    }
+
 }
--- src/test/java/com/example/alg/bitpkg/SingleNumberTest.java
+++ src/test/java/com/example/alg/bitpkg/SingleNumberTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SingleNumberTest {
+    final SingleNumber s = new SingleNumber();
+
+
+    @Test
+    public void singleNumber2() throws Exception {
+        assertEquals(4, s.singleNumber2(new int[]{2,2,2,4}));
+
+    }
+
     @Test
     public void singleNumber() throws Exception {
 
-        final SingleNumber s = new SingleNumber();
         assertEquals(1, s.singleNumber(new int[]{2,3,1,2,3}));
 
 
