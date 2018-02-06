f7cc1d0ad362a4bcf35380c7cea619c05536f392
eight9080 <eight9080@gmail.com>
2018-02-06 23:55:34 +0200

    int to roman

--- src/main/java/com/example/alg/stringpck/RomanNumbers.java
+++ src/main/java/com/example/alg/stringpck/RomanNumbers.java
@@ -44,4 +44,24 @@ public class RomanNumbers {
         }
         return number;
     }
+
+    /**
+     * Given an integer, convert it to a roman numeral.
+
+     Input is guaranteed to be within the range from 1 to 3999.
+     */
+    public String intToRoman(int num) {
+
+        String[] symbol = new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] value = new int[]{1000,900,500,400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder romanNumber = new StringBuilder();
+        for(int i=0; num!= 0; i++){
+            while(num >= value[i]){
+                num-=value[i];
+                romanNumber.append(symbol[i]);
+            }
+        }
+        return romanNumber.toString();
+    }
 }
--- src/test/java/com/example/alg/stringpck/RomanNumbersTest.java
+++ src/test/java/com/example/alg/stringpck/RomanNumbersTest.java
@@ -6,10 +6,26 @@ import static org.junit.Assert.*;
 
 public class RomanNumbersTest {
     @Test
+    public void intToRoman() throws Exception {
+        final RomanNumbers r = new RomanNumbers();
+        assertEquals("MMM", r.intToRoman(3000));
+        assertEquals("DCXXI", r.intToRoman(621));
+        assertEquals("I", r.intToRoman(1));
+        assertEquals("V", r.intToRoman(5));
+        assertEquals("IX", r.intToRoman(9));
+        assertEquals("X", r.intToRoman(10));
+        assertEquals("L", r.intToRoman(50));
+        assertEquals("C", r.intToRoman(100));
+        assertEquals("D", r.intToRoman(500));
+        assertEquals("M", r.intToRoman(1000));
+
+    }
+
+    @Test
     public void romanToInt() throws Exception {
 
         final RomanNumbers r = new RomanNumbers();
-
+        assertEquals(3000, r.romanToInt("MMM"));
         assertEquals(621, r.romanToInt("DCXXI"));
         assertEquals(1, r.romanToInt("I"));
         assertEquals(5, r.romanToInt("V"));
