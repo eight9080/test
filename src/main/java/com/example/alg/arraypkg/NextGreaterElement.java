2afa3414fa8021b8de2e9186f189cebd1d537e9f
eight9080 <eight9080@gmail.com>
2018-03-06 22:37:09 +0200

    next greater element II

--- src/main/java/com/example/alg/arraypkg/NextGreaterElement.java
+++ src/main/java/com/example/alg/arraypkg/NextGreaterElement.java
@@ -1,31 +1,27 @@
 package com.example.alg.arraypkg;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class NextGreaterElement {
 
     /**
      * You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
-
-     The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
-
-     Example 1:
-     Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
-     Output: [-1,3,-1]
-     Explanation:
-     For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
-     For number 1 in the first array, the next greater number for it in the second array is 3.
-     For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
-     Example 2:
-     Input: nums1 = [2,4], nums2 = [1,2,3,4].
-     Output: [3,-1]
-     Explanation:
-     For number 2 in the first array, the next greater number for it in the second array is 3.
-     For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
-
+     * <p>
+     * The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+     * <p>
+     * Example 1:
+     * Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+     * Output: [-1,3,-1]
+     * Explanation:
+     * For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+     * For number 1 in the first array, the next greater number for it in the second array is 3.
+     * For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+     * Example 2:
+     * Input: nums1 = [2,4], nums2 = [1,2,3,4].
+     * Output: [3,-1]
+     * Explanation:
+     * For number 2 in the first array, the next greater number for it in the second array is 3.
+     * For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
      */
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
@@ -33,9 +29,9 @@ public class NextGreaterElement {
 
         Deque<Integer> stack = new ArrayDeque<>();
 
-        for (int num: nums2) {
+        for (int num : nums2) {
 
-            while (!stack.isEmpty() && stack.peek()<num){
+            while (!stack.isEmpty() && stack.peek() < num) {
                 nextGreaterElement.put(stack.pop(), num);
             }
 
@@ -49,4 +45,68 @@ public class NextGreaterElement {
         return result;
     }
 
+
+    /**
+     * Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+     * <p>
+     * Example 1:
+     * Input: [1,2,1]
+     * Output: [2,-1,2]
+     * Explanation: The first 1's next greater number is 2;
+     * The number 2 can't find next greater number;
+     * The second 1's next greater number needs to search circularly, which is also 2.
+     */
+    public int[] nextGreaterElements(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{};
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            stack.push(i);
+        }
+
+        int[] result = new int[nums.length];
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] = -1;
+            while (!stack.isEmpty()) {
+                if (nums[stack.peek()] > nums[i]) {
+                    break;
+                }
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                result[i] = nums[stack.peek()];
+            }
+            stack.push(i);
+        }
+
+        return result;
+
+    }
+
+    public int[] nextGreaterElements2(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+//        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = n - 1; i >= 0; i--) {
+            stack.push(i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = -1;
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                result[i] = nums[stack.peek()];
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
 }
--- src/test/java/com/example/alg/arraypkg/NextGreaterElementTest.java
+++ src/test/java/com/example/alg/arraypkg/NextGreaterElementTest.java
@@ -16,4 +16,27 @@ public class NextGreaterElementTest {
         assertArrayEquals(new int[]{3,-1},
                 n.nextGreaterElement(new int[]{2,4}, new int[]{1,2,3,4}));
     }
+
+    @Test
+    public void nextGreaterElements() {
+
+        final NextGreaterElement b = new NextGreaterElement();
+        assertArrayEquals(new int[]{2,3,4,-1,4},
+                b.nextGreaterElements(new int[]{1,2,3,4,3}));
+        assertArrayEquals(new int[]{2,-1,2},
+                b.nextGreaterElements(new int[]{1,2,1}));
+        assertArrayEquals(new int[]{3,-1,3,3},
+                b.nextGreaterElements(new int[]{1,3,2,1}));
+    }
+    @Test
+    public void nextGreaterElements2() {
+
+        final NextGreaterElement b = new NextGreaterElement();
+        assertArrayEquals(new int[]{2,3,4,-1,4},
+                b.nextGreaterElements2(new int[]{1,2,3,4,3}));
+        assertArrayEquals(new int[]{2,-1,2},
+                b.nextGreaterElements2(new int[]{1,2,1}));
+        assertArrayEquals(new int[]{3,-1,3,3},
+                b.nextGreaterElements2(new int[]{1,3,2,1}));
+    }
 }
