a1f3ac392468a49d1b880d173190e388e02c2403
eight9080 <eight9080@gmail.com>
2018-04-28 00:11:18 +0300

    self divide integers

--- src/main/java/com/example/alg/mathpck/DivideIntegers.java
+++ src/main/java/com/example/alg/mathpck/DivideIntegers.java
@@ -1,5 +1,8 @@
 package com.example.alg.mathpck;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DivideIntegers {
     /**
      * Divide two integers without using multiplication, division and mod operator.
@@ -51,5 +54,44 @@ public class DivideIntegers {
         return multiplier+ partialResult;
 
 
+    }
+
+    /**
+     * A self-dividing number is a number that is divisible by every digit it contains.
+
+     For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+     Also, a self-dividing number is not allowed to contain the digit zero.
+
+     Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+     Example 1:
+     Input:
+     left = 1, right = 22
+     Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+     */
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = left; i <= right; i++) {
+            if(isSelfDivide(i)){
+                result.add(i);
+            }
+
+        }
+        return result;
+    }
+
+    private boolean isSelfDivide(int num) {
+        int currentNum = num;
+        while (currentNum!=0){
+            int digit = currentNum % 10;
+            if(digit==0 || (num % digit!=0)){
+                return false;
+            }
+            currentNum = currentNum/10;
+        }
+
+        return true;
     }
 }
--- src/test/java/com/example/alg/mathpck/DivideIntegersTest.java
+++ src/test/java/com/example/alg/mathpck/DivideIntegersTest.java
@@ -2,6 +2,9 @@ package com.example.alg.mathpck;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class DivideIntegersTest {
@@ -13,6 +16,16 @@ public class DivideIntegersTest {
         assertEquals(4, d.divide(8, 2));
 
 
+
+    }
+
+    @Test
+    public void selfDividingNumbers() {
+
+        final DivideIntegers d = new DivideIntegers();
+        final List<Integer> integers = d.selfDividingNumbers(1, 22);
+        final List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22);
+        assertEquals(expect, integers);
 
     }
 }
