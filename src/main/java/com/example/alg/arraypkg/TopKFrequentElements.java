dd61b1c6bd4f6219725f9d716a9a0779260fa228
eight9080 <eight9080@gmail.com>
2019-02-20 00:46:23 +0200

    top k words

--- src/main/java/com/example/alg/arraypkg/TopKFrequentElements.java
+++ src/main/java/com/example/alg/arraypkg/TopKFrequentElements.java
@@ -1,6 +1,7 @@
 package com.example.alg.arraypkg;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class TopKFrequentElements {
@@ -13,7 +14,7 @@ public class TopKFrequentElements {
         for (int key : frequencyMap.keySet()) {
             final Integer frequency = frequencyMap.get(key);
 
-            if(buckets[frequency]==null){
+            if (buckets[frequency] == null) {
                 buckets[frequency] = new ArrayList<>();
             }
             buckets[frequency].add(key);
@@ -21,13 +22,13 @@ public class TopKFrequentElements {
 
         final ArrayList<Integer> result = new ArrayList<>();
 
-        for(int i = buckets.length-1; i >=0; i--){
-            if(result.size()<=k) {
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            if (result.size() <= k) {
                 final List<Integer> elements = buckets[i];
-                if(elements!=null && !elements.isEmpty()) {
-                    if(elements.size()+result.size()<=k) {
+                if (elements != null && !elements.isEmpty()) {
+                    if (elements.size() + result.size() <= k) {
                         result.addAll(elements);
-                    }else{
+                    } else {
                         final int necessaryElements = k - result.size();
                         result.addAll(elements.stream().limit(necessaryElements).collect(Collectors.toList()));
                     }
@@ -35,6 +36,42 @@ public class TopKFrequentElements {
             }
         }
 
-            return result;
+        return result;
+    }
+
+    /**
+     * Given a non-empty list of words, return the k most frequent elements.
+     * <p>
+     * Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
+     * <p>
+     * Example 1:
+     * Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+     * Output: ["i", "love"]
+     * Explanation: "i" and "love" are the two most frequent words.
+     * Note that "i" comes before "love" due to a lower alphabetical order.
+     * Example 2:
+     * Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+     * Output: ["the", "is", "sunny", "day"]
+     * Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+     * with the number of occurrence being 4, 3, 2 and 1 respectively.
+     */
+    public List<String> topKFrequent(String[] words, int k) {
+
+        final Map<String, Integer> freq = Arrays.stream(words)
+                .collect(Collectors.toMap(Function.identity(), w -> 1, (a, b) -> a + 1));
+        final List<String> result = freq.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue(Comparator.reverseOrder())
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .limit(k)
+                .map(v -> v.getKey())
+                .collect(Collectors.toList());
+
+        return result;
+
+
+//        final PriorityQueue<String> wordsResult = new PriorityQueueiorityQueue<>();
+
+
     }
 }
--- src/test/java/com/example/alg/arraypkg/TopKFrequentElementsTest.java
+++ src/test/java/com/example/alg/arraypkg/TopKFrequentElementsTest.java
@@ -3,6 +3,7 @@ package com.example.alg.arraypkg;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,4 +18,21 @@ public class TopKFrequentElementsTest {
 
     }
 
+    @Test
+    public void topKFrequent_2words() {
+
+        final TopKFrequentElements t = new TopKFrequentElements();
+        final List<String> result = t.topKFrequent(new String[]{"i", "love", "leetcode", "i", "love", "coding"}, 2);
+        assertEquals(Arrays.asList("i", "love"), result);
+
+    }
+
+    @Test
+    public void topKFrequent_4words() {
+
+        final TopKFrequentElements t = new TopKFrequentElements();
+        final List<String> result = t.topKFrequent(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4);
+        assertEquals(Arrays.asList("the", "is", "sunny", "day"), result);
+
+    }
 }
