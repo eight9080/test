077175d273e0e3862b8b00583b29acb6e3e8d815
eight9080 <eight9080@gmail.com>
2020-02-03 00:39:06 +0200

    MyList high order functions fold zipWith sort foreach

--- src/main/scala/ro/dg/exercise/MyList.scala
+++ src/main/scala/ro/dg/exercise/MyList.scala
@@ -2,31 +2,44 @@ package ro.dg.exercise
 
 abstract class MyList[+A] {
 
-  def head : A
-  def tail : MyList[A]
+  def head: A
+
+  def tail: MyList[A]
 
   def isEmpty: Boolean
-  def add[B>:A](element: B): MyList[B]
 
-  def printElements : String
-  override def toString: String = "["+ printElements+"]"
+  def add[B >: A](element: B): MyList[B]
+
+  def printElements: String
+
+  override def toString: String = "[" + printElements + "]"
 
   def map[B](transformer: A => B): MyList[B]
-  def filter(predicate: A => Boolean) : MyList[A]
+
+  def filter(predicate: A => Boolean): MyList[A]
+
   def flatMap[B](transformer: A => MyList[B]): MyList[B]
 
-  def ++[B>:A](list: MyList[B]) : MyList[B]
+  def ++[B >: A](list: MyList[B]): MyList[B]
+
+  def foreach(f: A => Unit): Unit
+
+  def sort(comparator: (A, A) => Int): MyList[A]
+
+  def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C]
+
+  def fold[B](start: B)(operator: (B,A)=> B): B
 
 }
 
-case object Empty extends MyList[Nothing]{
+case object Empty extends MyList[Nothing] {
   override def head: Nothing = throw new NoSuchElementException
 
   override def tail: MyList[Nothing] = throw new NoSuchElementException
 
   override def isEmpty: Boolean = true
 
-  override def add[B>:Nothing](element: B): MyList[B] = new Cons(element, Empty)
+  override def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
 
   override def printElements: String = ""
 
@@ -37,9 +50,21 @@ case object Empty extends MyList[Nothing]{
   override def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
 
   override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
+
+  override def foreach(f: Nothing => Unit): Unit = ()
+
+  override def sort(comparator: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
+
+  override def zipWith[B, C](list: MyList[B], zip: (Nothing, B) => C): MyList[C] = {
+    if (!list.isEmpty) throw new RuntimeException("List do not have the same length")
+    else Empty
+  }
+
+  override def fold[B](start: B)(operator: (B, Nothing) => B): B = {start}
+
 }
 
-case class Cons[+A](h:A, t: MyList[A]) extends MyList[A]{
+case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
   override def head: A = h
 
@@ -47,18 +72,18 @@ case class Cons[+A](h:A, t: MyList[A]) extends MyList[A]{
 
   override def isEmpty: Boolean = false
 
-  override def add[B>:A](element: B): MyList[B] = new Cons(element, this)
+  override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
   override def printElements: String =
-    if(t.isEmpty) ""+h
-    else h +" "+t.printElements
+    if (t.isEmpty) "" + h
+    else h + " " + t.printElements
 
   override def map[B](transformer: A => B): MyList[B] = {
     new Cons(transformer(h), t.map(transformer))
   }
 
   override def filter(predicate: A => Boolean): MyList[A] = {
-    if(predicate(h)) new Cons(h, t.filter(predicate))
+    if (predicate(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
   }
 
@@ -67,9 +92,33 @@ case class Cons[+A](h:A, t: MyList[A]) extends MyList[A]{
   }
 
   override def ++[B >: A](list: MyList[B]): MyList[B] = {
-    new Cons[B](h, t++list)
+    new Cons[B](h, t ++ list)
   }
 
+  override def foreach(f: A => Unit): Unit = {
+    f(h)
+    t.foreach(f)
+  }
+
+  override def sort(comparator: (A, A) => Int): MyList[A] = {
+    def insert(x: A, sortedList: MyList[A]): MyList[A] = {
+      if (sortedList.isEmpty) new Cons(x, Empty)
+      else if (comparator(x, sortedList.head) <= 0) new Cons(x, sortedList)
+      else new Cons(sortedList.head, insert(x, sortedList.tail))
+    }
+
+    val sortedTail = t.sort(comparator)
+    insert(h, sortedTail)
+  }
+
+  override def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C] = {
+    if (list.isEmpty) throw new RuntimeException("List do not have the same length")
+    else new Cons(zip(h, list.head), t.zipWith(list.tail, zip))
+  }
+
+  override def fold[B](start: B)(operator: (B, A) => B): B = {
+     t.fold(operator(start, h))(operator)
+  }
 }
 
 //trait MyPredicate[-T]{
@@ -88,13 +137,13 @@ case class Cons[+A](h:A, t: MyList[A]) extends MyList[A]{
 //  override def transform(value: String): Int = value.toInt
 //}
 
-object ListTest extends App{
+object ListTest extends App {
 
-//  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-//  println(list.tail.head)
-//  println(list.add(5).head)
-//  println(list.isEmpty)
-//  println(list.toString)
+  //  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  //  println(list.tail.head)
+  //  println(list.add(5).head)
+  //  println(list.isEmpty)
+  //  println(list.toString)
 
   val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
   val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
@@ -109,13 +158,20 @@ object ListTest extends App{
 
   println(listOfIntegers.filter(_ % 2 == 0))
 
-  println(listOfIntegers++anotherListOfIntegers)
+  println(listOfIntegers ++ anotherListOfIntegers)
 
-  println(listOfIntegers.flatMap(value =>  new Cons[Int](value, new Cons[Int](value+1, Empty))).toString)
+  println(listOfIntegers.flatMap(value => new Cons[Int](value, new Cons[Int](value + 1, Empty))).toString)
 
-//  def concatenator: (String, String) => String = new Function2[String, String, String] {
-//    override def apply(v1: String, v2: String): String = v1+v2
-//  }
-//  println(concatenator("1","20"))
+  listOfIntegers.foreach(println)
+
+  println(listOfIntegers.sort((a, b) => b - a).toString)
+
+  println(anotherListOfIntegers.zipWith[String, String](listOfString, _+"-"+_))
+  println(listOfIntegers.fold(1)((a,b) => a+b))
+
+  //  def concatenator: (String, String) => String = new Function2[String, String, String] {
+  //    override def apply(v1: String, v2: String): String = v1+v2
+  //  }
+  //  println(concatenator("1","20"))
 
 }
